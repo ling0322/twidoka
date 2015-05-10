fa0cb3acc3ed4a6664d6735b30fdbe86cabbf000
Ubuntu <azureuser@yuki02.yuki02.m6.internal.cloudapp.net>
2015-05-10 17:07:27 +0000

    Re-add the .go files

---
+++ twidoka.go
@@ -0,0 +1,312 @@
+package main
+
+import (
+  "net/http"
+  "net/url"
+  "github.com/ChimeraCoder/anaconda"
+  "fmt"
+  // "time"
+  "github.com/garyburd/go-oauth/oauth"
+  // "html/template"
+  "strconv"
+  "net/http/httputil"
+  "strings"
+)
+
+const (
+  OK = 0
+  RetrieveCount = 200
+)
+
+func main() {
+  http.HandleFunc("/", root(signinRequired(partial(timelineHandler, "Home"))))
+  http.HandleFunc("/mentions", signinRequired(partial(timelineHandler, "Mentions")))
+  http.HandleFunc("/user", signinRequired(partial(timelineHandler, "User")))
+  http.HandleFunc("/search", signinRequired(partial(timelineHandler, "Search")))
+  http.HandleFunc("/signin", signInHandler)
+  http.HandleFunc("/signout", signOutHandler)
+  http.HandleFunc("/update", signinRequired(updateHandler))
+  http.HandleFunc("/details", signinRequired(detailsHandler))
+  http.HandleFunc("/reply", signinRequired(partial(composeHandler, "Reply")))
+  http.HandleFunc("/retweet", signinRequired(partial(composeHandler, "Retweet")))
+  http.HandleFunc("/compose", signinRequired(partial(composeHandler, "Compose")))
+  http.HandleFunc("/remove", signinRequired(removeHandler))
+  http.HandleFunc("/authorize", authorizeHandler)
+  http.Handle("/static/", http.FileServer(http.Dir(".")))
+  http.HandleFunc("/oauth_token", oauthTokenHandler)
+
+  // Add a reverse proxy for images
+  dstUrl, _ := url.Parse("http://pbs.twimg.com/")
+  reversedProxy := httputil.NewSingleHostReverseProxy(dstUrl)
+  http.HandleFunc("/p/", func(w http.ResponseWriter, r *http.Request) {
+    r.Host = "pbs.twimg.com"
+    // Remove prefix `/p`
+    r.URL.Path = r.URL.Path[2: ]
+    reversedProxy.ServeHTTP(w, r)
+  })
+
+  anaconda.SetConsumerKey(ConsumerKey)
+  anaconda.SetConsumerSecret(ConsumerSecret)
+
+  http.ListenAndServe(":80", nil)
+}
+
+func signOutHandler(w http.ResponseWriter, r *http.Request) {
+  deleteCookie(w, "access_token")
+  deleteCookie(w, "access_token_secret")
+  deleteCookie(w, "screen_name")
+  http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func removeHandler(w http.ResponseWriter, r *http.Request) {
+  var err error
+  id, err := strconv.ParseInt(r.FormValue("id"), 10, 64)
+  confirm := r.FormValue("confirm")
+  screenName := getCookie(r, "screen_name")
+  api := buildAnacondaApiFromRequest(r)
+  defer api.Close()
+
+  if confirm == "" {
+    // Not yet confirmed
+    var tweet anaconda.Tweet
+    if err == nil {
+      tweet, err = api.GetTweet(id, url.Values{})
+    }
+    if err == nil {
+      view := convertToTweetView(&tweet, screenName, true)
+      removeTemplate.Execute(w, &removeView{
+          Tweet: view,
+          Referer: r.Referer()})
+    }
+  } else {
+    // Confirmed, remove it!
+    if err == nil {
+      _, err = api.DeleteTweet(id, true)
+    }
+    if err == nil {
+      referer := r.FormValue("referer")
+      if referer == "" {
+        referer = "/"
+      }
+      http.Redirect(w, r, referer, http.StatusFound)
+    }
+  }
+
+  if err != nil {
+    errorHandler(w, err)
+  }
+}
+
+func signInHandler(w http.ResponseWriter, r *http.Request) {
+  signInTemplate.Execute(w, nil)
+}
+
+func updateHandler(w http.ResponseWriter, r *http.Request) {
+  api := buildAnacondaApiFromRequest(r)
+  defer api.Close()
+
+  text := r.FormValue("text")
+  inReplyTo := r.FormValue("in_reply_to")
+
+  values := url.Values{}
+  if inReplyTo != "" {
+    values.Add("in_reply_to_status_id", inReplyTo)
+  }
+
+  _, err := api.PostTweet(text, values)
+  if err == nil {
+    referer := r.FormValue("referer")
+    if referer == "" {
+      referer = "/"
+    }
+    http.Redirect(w, r, referer, http.StatusFound)
+  } else {
+    errorHandler(w, err)
+  }
+}
+
+func composeHandler(composeType string, w http.ResponseWriter, r *http.Request) {
+  screenName := getCookie(r, "screen_name")
+  api := buildAnacondaApiFromRequest(r)
+  defer api.Close()
+
+  var err error
+  var id int64
+  var tweet anaconda.Tweet
+  if composeType != "Compose" {
+    id, err = strconv.ParseInt(r.FormValue("id"), 10, 64)
+    if err == nil {
+      tweet, err = api.GetTweet(id, url.Values{})
+    }
+  }
+
+  if err == nil {
+    view := convertToTweetView(&tweet, screenName, true)
+    compose := new(composeView)
+    compose.ScreenName = screenName
+    compose.Referer = r.Referer()
+    switch composeType {
+    case "Compose":
+      compose.DefaultText = ""
+    case "Reply":
+      replyTo := []string{"@" + view.ScreenName}
+      for _, mentionedUser := range view.Mentioned {
+        if mentionedUser != screenName {
+          replyTo = append(replyTo, "@" + mentionedUser)
+        }
+      }
+      compose.DefaultText = fmt.Sprintf("%s ", strings.Join(replyTo, " "))
+      compose.InReplyToTweet = view
+    case "Retweet":
+      compose.DefaultText = fmt.Sprintf(" RT @%s: %s", view.ScreenName, tweet.Text)
+    }
+    compose.Type = composeType
+    composeTemplate.Execute(w, compose)
+  }
+
+  if err != nil {
+    errorHandler(w, err)
+  }
+}
+
+func detailsHandler(w http.ResponseWriter, r *http.Request) {
+  screenName := getCookie(r, "screen_name")
+  api := buildAnacondaApiFromRequest(r)
+  defer api.Close()
+
+  var err error
+  id, err := strconv.ParseInt(r.FormValue("id"), 10, 64)
+
+  var tweet anaconda.Tweet
+  if err == nil {
+    tweet, err = api.GetTweet(id, url.Values{})
+  }
+
+  var view *tweetView
+  var details *detailsView
+  var inReplyToView *tweetView
+  if err == nil {
+    view = convertToTweetView(&tweet, screenName, true)
+    if view.InReplyToStatusId > 0 {
+      inReplyToTweet, e := api.GetTweet(view.InReplyToStatusId, url.Values{})
+      if e == nil {
+        inReplyToView = convertToTweetView(&inReplyToTweet, screenName, true)
+      }
+    }
+    view.ShowFull = true
+
+    details = new(detailsView)
+    details.InReplyTo = inReplyToView
+    details.Tweet = view
+    details.ScreenName = screenName
+  }
+
+  if err == nil {
+    err = detailsTemplate.Execute(w, details)
+  }
+
+  if err != nil {
+    errorHandler(w, err)
+  }
+}
+
+func timelineHandler(timelineType string, w http.ResponseWriter, r *http.Request) {
+  screenName := getCookie(r, "screen_name")
+  api := buildAnacondaApiFromRequest(r)
+  defer api.Close()
+  values := buildTimelineRequestValues(r)
+
+  // Only in user timeline mode
+  userScreenName := r.FormValue("u")
+
+  // Only in search mode
+  query := r.FormValue("q")
+
+  // Gets the timeline from twitter.com
+  var err error
+  var timeline []anaconda.Tweet
+  switch timelineType {
+  case "Home":
+    timeline, err = api.GetHomeTimeline(values)
+  case "Mentions":
+    timeline, err = api.GetMentionsTimeline(values)
+  case "User":
+    values.Add("screen_name", userScreenName)
+    timeline, err = api.GetUserTimeline(values)
+  case "Search":
+    if query == "" {
+      timeline = make([]anaconda.Tweet, 0)
+    } else {
+      var searchResponse anaconda.SearchResponse
+      searchResponse, err = api.GetSearch(query, values)
+      timeline = searchResponse.Statuses
+    }
+  }
+
+  // Only in user timeline mode
+  var userInf anaconda.User
+  var user *userView
+  if err == nil && timelineType == "User" {
+    userInf, err = api.GetUsersShow(userScreenName, url.Values{})
+  }
+  if err == nil && timelineType == "User" {
+    user = convertToUserView(&userInf)
+  }
+
+  if err == nil {
+    tweetViews := make([]*tweetView, len(timeline))
+    for i := range timeline {
+      tweetViews[i] = convertToTweetView(&timeline[i], screenName, true)
+    }
+
+    var lastId int64
+    if len(timeline) > 0 {
+      lastId = timeline[len(timeline) - 1].Id
+    }
+
+    timelineTemplate.Execute(w, &timelineView{
+        Title: timelineType,
+        ScreenName: screenName,
+        SinceId: lastId,
+        User: user,
+        Tweets: tweetViews,
+        Search: query})
+  }
+
+  if err != nil {
+    errorHandler(w, err)
+  }
+}
+
+func authorizeHandler(w http.ResponseWriter, r *http.Request) {
+  authUrl, credentials, err := anaconda.AuthorizationURL(SiteURL + "/oauth_token")
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  } else {
+    setCookie(w, "access_token", credentials.Token)
+    setCookie(w, "access_token_secret", credentials.Secret)
+    http.Redirect(w, r, authUrl, http.StatusFound)
+  }
+}
+
+func oauthTokenHandler(w http.ResponseWriter, r *http.Request) {
+  token := getCookie(r, "access_token")
+  secret := getCookie(r, "access_token_secret")
+  tempCred := &oauth.Credentials{token, secret}
+
+  verifier := r.FormValue("oauth_verifier")
+  credentials, values, err := anaconda.GetCredentials(tempCred, verifier)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  } else {
+    deleteCookie(w, "access_token")
+    deleteCookie(w, "access_token_secret")
+
+    url := fmt.Sprintf(
+        "?access_token=%s&access_token_secret=%s&screen_name=%s",
+        credentials.Token,
+        credentials.Secret,
+        values["screen_name"][0])
+    http.Redirect(w, r, url, http.StatusFound)
+  }
+}
---
+++ utils.go
@@ -0,0 +1,217 @@
+package main
+
+import (
+  "net/http"
+  "github.com/ChimeraCoder/anaconda"
+  "time"
+  "fmt"
+  "strconv"
+  "html/template"
+  "net/url"
+  "strings"
+)
+
+type notFoundError struct {
+}
+func (e *notFoundError) Error() string {
+  return "Page not found"
+}
+
+func errorHandler(w http.ResponseWriter, err error) {
+  var errno int
+  switch err.(type) {
+  case *notFoundError:
+    errno = http.StatusNotFound
+  default:
+    errno = http.StatusInternalServerError
+  }
+
+  w.WriteHeader(errno)
+  errorTemplate.Execute(w, err)
+}
+
+func root(handler func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
+  return func(w http.ResponseWriter, r *http.Request) {
+    // Hack: set the access_token cookies and add them into request
+    accessToken := r.FormValue("access_token")
+    if accessToken != "" {
+      setCookie(w, "access_token", accessToken)
+      r.AddCookie(buildCookie("access_token", accessToken))
+    }
+    accessTokenSecret := r.FormValue("access_token_secret")
+    if accessTokenSecret != "" {
+      setCookie(w, "access_token_secret", accessTokenSecret)
+      r.AddCookie(buildCookie("access_token_secret", accessTokenSecret))
+    }
+    screenName := r.FormValue("screen_name")
+    if screenName != "" {
+      setCookie(w, "screen_name", screenName)
+      r.AddCookie(buildCookie("screen_name", screenName))
+    }
+
+    path := r.URL.Path
+    if path != "/" {
+      errorHandler(w, &notFoundError{})
+    } else {
+      handler(w, r)
+    }
+  }
+}
+
+func signinRequired(handler func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
+  return func(w http.ResponseWriter, r *http.Request) {
+    token := getCookie(r, "access_token")
+    if token == "" {
+      http.Redirect(w, r, "/signin", http.StatusFound)
+    } else {
+      handler(w, r)
+    }
+  }
+}
+
+func partial(handler func(string, http.ResponseWriter, *http.Request), handlerType string) func(http.ResponseWriter, *http.Request) {
+  return func(w http.ResponseWriter, r *http.Request) {
+    handler(handlerType, w, r)
+  }
+}
+
+func buildAnacondaApiFromRequest(r *http.Request) *anaconda.TwitterApi {
+  token := getCookie(r, "access_token")
+  secret := getCookie(r, "access_token_secret")
+  api := anaconda.NewTwitterApi(token, secret)
+  api.ReturnRateLimitError(true)
+
+  return api
+}
+
+func buildTimelineRequestValues(r *http.Request) url.Values {
+  values := url.Values{}
+
+  maxId, err := strconv.ParseInt(r.FormValue("max_id"), 10, 64)
+  if err == nil {
+    values.Add("max_id", strconv.FormatInt(maxId - 1, 10))
+  }
+  values.Add("count", strconv.Itoa(RetrieveCount))
+
+  return values
+}
+
+func tweetTextHtml(t *anaconda.Tweet) string {
+  html := t.Text
+
+  for _, url := range t.Entities.Urls {
+    htmlA := fmt.Sprintf(`<a href="%s">%s</a>`, url.Expanded_url, url.Display_url)
+    html = strings.Replace(html, url.Url, htmlA, -1)
+  }
+
+  for _, user := range t.Entities.User_mentions {
+    htmlA := fmt.Sprintf(`<a href="/user?u=%s">@%s</a>`, user.Screen_name, user.Screen_name)
+    html = strings.Replace(html, "@" + user.Screen_name, htmlA, -1)
+  }
+
+  for _, media := range t.Entities.Media {
+    htmlA := fmt.Sprintf(`<a href="%s">%s</a>`, media.Expanded_url, media.Display_url)
+    html = strings.Replace(html, media.Url, htmlA, -1)
+  }
+
+  return html
+}
+
+func convertToUserView(u *anaconda.User) *userView {
+  user := new(userView)
+  user.ProfileImageUrl = u.ProfileImageURL
+  user.ScreenName = u.ScreenName
+  user.Name = u.Name
+  user.Description = u.Description
+  user.FriendsCount = u.FriendsCount
+  user.FollowersCount = u.FollowersCount
+  user.StatusesCount = u.StatusesCount
+  user.Location = u.Location
+  user.Following = u.Following
+  createdAt, _ := time.Parse(time.RubyDate, u.CreatedAt)
+  user.CreatedAt = timeToReadableString(createdAt)
+  return user
+}
+
+func convertToTweetView(t *anaconda.Tweet, screenName string, showOperator bool) *tweetView {
+  tweet := new(tweetView)
+  tweet.ScreenName = t.User.ScreenName
+  tweet.Name = t.User.Name
+  tweet.InReplyToStatusId = t.InReplyToStatusID
+  tweet.Id = t.Id
+  tweet.ProfileImageUrl = strings.Replace(
+      t.User.ProfileImageURL,
+      "http://pbs.twimg.com",
+      "/p",
+      -1);
+  tweet.Text = template.HTML(tweetTextHtml(t))
+  tweet.Source = template.HTML(t.Source)
+  if screenName == t.User.ScreenName {
+    tweet.ShowRemove = true
+  } else {
+    tweet.ShowRemove = false
+  }
+  tweet.ShowOperator = showOperator
+  createdAt, _ := t.CreatedAtTime()
+  tweet.CreateTime = timeToReadableString(createdAt)
+
+  // Mentioned users
+  tweet.Mentioned = []string{}
+  for _, user := range t.Entities.User_mentions {
+    tweet.Mentioned = append(tweet.Mentioned, user.Screen_name)
+  }
+  
+  // Extract image in tweet
+  for _, media := range t.Entities.Media {
+    tweet.ImageUrl = media.Media_url_https
+  }
+  return tweet
+}
+
+func timeToReadableString(t time.Time) string {
+  d := time.Since(t)
+  if t.Year() != time.Now().Year() {
+    return t.Format("2006-01-02")
+  } else if d.Hours() > 24 {
+    return t.Format("01-02")
+  } else if d.Hours() > 1 {
+    return fmt.Sprintf("%dh ago", int(d.Hours()))
+  } else if d.Minutes() > 1 {
+    return fmt.Sprintf("%dm ago", int(d.Minutes()))
+  } else {
+    return fmt.Sprintf("%ds ago", int(d.Seconds()))
+  }
+}
+
+func buildCookie(key string, value string) *http.Cookie {
+  expires := time.Date(2099, time.November, 10, 23, 0, 0, 0, time.UTC)
+  return &http.Cookie{
+      Name: key,
+      Value: value,
+      Expires: expires,
+      Path: "/",
+      HttpOnly: true }
+}
+
+func setCookie(w http.ResponseWriter, key string, value string) {
+  cookie := buildCookie(key, value)
+  http.SetCookie(w, cookie)
+}
+
+func deleteCookie(w http.ResponseWriter, key string) {
+  http.SetCookie(w, &http.Cookie{
+    Name: key,
+    Value: "deleted",
+    MaxAge: -1,
+    Path: "/",
+    HttpOnly: true })
+}
+
+func getCookie(r *http.Request, key string) string {
+  cookie, err := r.Cookie(key)
+  if err != nil {
+    return ""
+  } else {
+    return cookie.Value
+  }
+}
